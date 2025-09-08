d41dd0b83709b776641bbba66ef83f6baa3ff930
Himans <himansbhardwaj1566@gmail.com>
2025-09-08 13:26:34 +0530

    add more files

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/react.svg";
+import countries from "./countries";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
   const [showCountryDropdown, setShowCountryDropdown] = useState(false);
+  const [showCategoryDropdown, setShowCategoryDropdown] = useState(false);
+  const [theme, setTheme] = useState("light-theme");
+  const categories = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sport",
+    "technology",
+  ];
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+  const toggleTheme = () => {
+    if (theme == "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
   return (
     <nav className="fixed top-0 left-0 w-full h-auto bg-gray-800 z-10 flex items-center justify-around">
       <h3 className="relative heading font-bold md:basis-1/6 text-2xl xs:basis-4/12 z-50 mb-2 mt-5">
@@ -34,9 +58,96 @@ const Navbar = () => {
             onClick={() => {
               setShowCountryDropdown(!showCountryDropdown);
             }}
-          ></Link>
+          >
+            Country
+          </Link>
+          <ul
+            className={
+              showCountryDropdown
+                ? "dropdown p-2 show-dropdown"
+                : "dropdown p-2"
+            }
+          >
+            {countries.map((ele, idx) => {
+              <li
+                key={idx}
+                onClick={() => {
+                  setShowCountryDropdown(!showCountryDropdown);
+                }}
+              >
+                <Link
+                  to={"/country/" + ele?.iso_2_alpha}
+                  className="flex gap-3"
+                  type="btn"
+                  onClick={() => {
+                    setActive(!active);
+                  }}
+                >
+                  <img
+                    src={ele?.png}
+                    srcSet={`https://flagcdn.com/32x24/${ele?.iso_2_alpha}.png 2x`}
+                    alt={ele?.countryName}
+                  />
+                  <span>{ele?.countryName}</span>
+                </Link>
+              </li>;
+            })}
+          </ul>
         </li>
-        <li></li>
+        <li className="dropdown-li">
+          <Link
+            className="no-underline font-semibold flex items-center gap-2"
+            onClick={() => {
+              setShowCategoryDropdown(!showCategoryDropdown);
+            }}
+            Top
+            Headlines
+          ></Link>
+          <ul
+            className={
+              showCategoryDropdown
+                ? "dropdown p-2 show-dropdown"
+                : "dropdown p-2"
+            }
+          >
+            {categories.map((ele, idx) => {
+              return (
+                <li
+                  key={idx}
+                  onClick={() => {
+                    setShowCategoryDropdown(!showCategoryDropdown);
+                  }}
+                >
+                  <Link
+                    to={"/top-headlines/" + ele}
+                    className="flex gap-3 capitalize"
+                    type="btn"
+                    onClick={() => {
+                      setActive(!active);
+                    }}
+                  >
+                    {ele}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </li>
+
+        <li>
+          <Link
+            className="no-underline font-semibold"
+            to="#"
+            onClick={toggleTheme}
+          >
+            <input type="checkbox" className="checkbox" id="checkbox" />
+            <label for="checkbox" class="checkbox-label">
+              <i className="fas fa-moon"></i>
+              <i className="fas fa-sun"></i>
+              <span className="ball"></span>
+            </label>
+          </Link>
+        </li>
         <li></li>
       </ul>
     </nav>
